2ac42bb26e4b8924636936239e4f68342ff8ca99
karlo-kasi <karlokasi1995@gmail.com>
2025-02-28 17:31:46 +0100

    Add: set up pages e layout app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//pages
+import HomePage from "./pages/Homepage"
+import ContactPage from "./pages/ContactPage"
+import Products from "./pages/Products"
+import Product from "./pages/Product"
+
+
+//layouts
+import DefaultLayout from "./layouts/DefaultLayout"
+
 function App() {
-  
+
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={HomePage} />
+            <Route path="/contact" Component={ContactPage} />
+            <Route path="/products" Component={Products} />
+            <Route path="/products/:id" Component={Product} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
